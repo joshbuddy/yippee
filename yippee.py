e3bddf4ea4671834017cd67cb92b89baa73f98a5
joshbuddy <joshbuddy@gmail.com>
2019-01-10 18:34:53 +0100

    Add flake8

--- yippee.py
+++ yippee.py
@@ -5,6 +5,7 @@ pip("click", "~=7.0")
 with group("development"):
     pip("twine", "~=1.12.0")
     pip("black", "18.9b0")
+    pip("flake8", "~=3.6.0")
     pip("wheel")
 
 # run pipecleaner to generate three files:
